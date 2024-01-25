49c2f1c469e5699fa65c7cc1c128a820e14d78a2
Boaz Kaufman <bk1nyc@gmail.com>
2024-01-25 15:41:08 -0500

    Edit setup settings; add description and url

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name="BackEndCore",
     version="1.0dev",
+    description="Core code for DataMixMaster's backends.",
     packages=find_packages(),
+    url="https://github.com/AthenaKouKou/BackEndCore",
+    install_requires=[],
 )
