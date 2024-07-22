13768497e7e55d6be7301ebb1e8c1a7c07b36244
gcallah <gcallah@mac.com>
2024-07-22 09:28:16 -0400

    valid.py added to core common.

--- backendcore/common/valid.py
+++ backendcore/common/valid.py
@@ -5,12 +5,20 @@ Things like emails and urls.
 NOT things like API records that are DMM specific.
 """
 
-import validators as vld
+import validators as vd
 
 
 def is_valid_url(url):
-    return vld.url(url)
+    return vd.url(url)
 
 
 def is_valid_email(email):
-    return vld.email(email)
+    return vd.email(email)
+
+
+def main():
+    print(f'{is_valid_email("gcallah@mac.com")=}')
+
+
+if __name__ == '__main__':
+    main()
