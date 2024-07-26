b2078e226b63f427bf4715bda1660d7c5e3ce427
gcallah <gcallah@mac.com>
2024-07-26 00:55:19 -0400

    Added api/constants.py: starting to collect common API constants.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BackEndCore",
-    version="1.2",
+    version="1.3",
     description="Core code for DataMixMaster's backends.",
     packages=find_packages(),
     url="https://github.com/AthenaKouKou/BackEndCore",
