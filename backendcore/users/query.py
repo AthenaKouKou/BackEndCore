449cf00a404b910f2cc701b5c3ca985c0d8f77a6
gcallah <gcallah@mac.com>
2024-07-29 15:06:08 -0400

    Adding user query module to backend core.

---
+++ backendcore/users/query.py
@@ -0,0 +1,316 @@
+"""
+This is our interface to our user data.
+AT PRESENT, the user's id is their email.
+We should not assume that is true forever!
+Thus, we should cut over from calling the ID
+field `email` to calling it `user_id`.
+"""
+import backendcore.common.valid as vld
+from backendcore.common.constants import EMAIL
+import backendcore.data.db_connect as dbc
+from backendcore.users.edit_groups import EDIT_GRP_FLD, TEST_NAME
+import backendcore.common.time_fmts as tfmt
+
+
+TEST_EDIT_GRP = TEST_NAME
+
+SUCCESS = 0
+
+USER_COLLECT = 'users'
+LOGIN_COLLECT = 'logins'
+FIRST_NM = 'first_name'
+LAST_NM = 'last_name'
+USER_ID = EMAIL
+SALT = 'salt'
+PASSWD = 'password'
+ORG = 'org'
+KEY = 'key'
+LOGINS = 'logins'
+ISSUE_TIME = 'issue_time'
+PW_RES_TOK = 'pw_reset_tok'
+PW_RES_TOK_ISS_TIME = 'pw_reset_tok_issue_time'
+PW_RES_SALT = 'pw_reset_salt'
+PAY_PROV_ID = 'pay_prov_id'
+PAY_PROV_SID = 'pay_prov_session_id'
+PAY_PROV_USER_ID = 'pay_prov_user_id'
+# some users may record email addresses they wish to send reports to:
+RPT_RECIPS = 'rpt_recipients'
+
+
+def list_users():
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    users = dbc.fetch_all(db_nm, USER_COLLECT)
+    user_list = []
+    for user in users:
+        last = get_last_login(user[USER_ID])
+        user_list.append({'id': user[USER_ID], 'last login': last})
+    return user_list
+
+
+def fetch_user(user_id: str):
+    """
+    User IDs must be unique in our db, so we can fetch a unique
+    record based on just user_id.
+    """
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    return dbc.fetch_one(db_nm, USER_COLLECT,
+                         filters={EMAIL: user_id})
+
+
+def create_user(email: str, firstname: str, lastname: str,
+                passwd: str, salt: str, org: str = None):
+    """
+    Creates a new user.
+    If the user email exists, raise ValueError.
+    Otherwise, return the new user's id.
+    """
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    if exists(email):
+        raise ValueError(f'{email=} already exists in userDB')
+    else:
+        return dbc.insert_doc(db_nm, USER_COLLECT,
+                              {EMAIL: email,
+                               FIRST_NM: firstname,
+                               LAST_NM: lastname,
+                               PASSWD: passwd,
+                               SALT: salt,
+                               ORG: org,
+                               KEY: "",
+                               LOGINS: [tfmt.get_today()],
+                               RPT_RECIPS: [],
+                               ISSUE_TIME: tfmt.get_today(),
+                               PW_RES_TOK: "",
+                               PW_RES_TOK_ISS_TIME: "",
+                               PW_RES_SALT: ""})
+
+
+def del_user(user_id: str):
+    """
+    Deletes a user.
+    If the user user_id doesn't exist, raise ValueError.
+    Otherwise, return SUCCESS
+    """
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    if not exists(user_id):
+        raise ValueError(f'{user_id=} is not in the database.')
+    else:
+        dbc.del_one(db_nm, USER_COLLECT, {EMAIL: user_id})
+        return SUCCESS
+
+
+def exists(user_id: str):
+    """
+    Is this id already in the user DB?
+    Returns True is so, else False.
+    """
+    return fetch_user(user_id) is not None
+
+
+def user_exists(user_id: str):
+    """
+    Deprecated: use `exists` instead.
+    """
+    return exists(user_id)
+
+
+def get_users_edit_grp(user_id: str):
+    edit_grp = None
+    user = fetch_user(user_id)
+    print(f'{user=}')
+    if user:
+        edit_grp = user.get(EDIT_GRP_FLD, None)
+    return edit_grp
+
+
+def add_login(user_id: str, date=None):
+    """
+    Records a new login.
+    Potentially passing a date is useful for testing.
+    """
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    if exists(user_id):
+        print(f'adding login for {user_id=}')
+        if date is None:
+            date = tfmt.get_today()
+        dbc.append_to_list(db_nm, LOGIN_COLLECT, EMAIL, user_id, LOGINS, date)
+
+
+def fetch_logins(user_id: str):
+    """
+    Fetches all logins for a user as a list.
+    """
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    print('Calling fetch_logins')
+    if exists(user_id):
+        return dbc.fetch_one(db_nm, LOGIN_COLLECT,
+                             filters={EMAIL: user_id})
+
+
+def get_last_login(user_id):
+    logins = fetch_logins(user_id)
+    if logins and len(logins[LOGINS]):
+        return logins[LOGINS][-1]
+    else:
+        return ''
+
+
+def add_rpt_recip(user_id: str, rec_email: str):
+    """
+    Adds a report recipient to a user doc.
+    Return the new list of recipients, or None
+    if the user doesn't exist.
+    Of course the `else` clause isn't needed...
+    but it makes the code clearer.
+    """
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    if not isinstance(rec_email, str):
+        raise TypeError(f'Recipient email must be a str; {rec_email=}')
+    if not vld.is_valid_email(rec_email):
+        raise ValueError(f'Invalid recipient email: {rec_email=}')
+    user = fetch_user(user_id)
+    if user:
+        if RPT_RECIPS not in user:
+            user[RPT_RECIPS] = []
+        user[RPT_RECIPS].append(rec_email)
+        dbc.update_fld(db_nm, USER_COLLECT, {EMAIL: user_id},
+                       RPT_RECIPS, user[RPT_RECIPS])
+        return user[RPT_RECIPS]
+    else:
+        return None
+
+
+def replace_rpt_recips(user_id: str, recips: list):
+    """
+    Adds a report recipient to a user doc.
+    Return the new list of recipients, or None
+    if the user doesn't exist.
+    Of course the `else` clause isn't needed...
+    but it makes the code clearer.
+    """
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    if not isinstance(recips, list):
+        raise TypeError(f'Recipients must be a list: {recips=}')
+    if exists(user_id):
+        ret = dbc.update_fld(db_nm, USER_COLLECT, {EMAIL: user_id},
+                             RPT_RECIPS, recips)
+        return ret
+    else:
+        return None
+
+
+def get_rpt_recips(user_id: str):
+    """
+    Lists report recipients for a user.
+    """
+    user = fetch_user(user_id)
+    if user:
+        if RPT_RECIPS not in user:
+            user[RPT_RECIPS] = []
+        return user[RPT_RECIPS]
+    else:
+        return None
+
+
+def update_pw_reset_token(user_id: str, salt: str, hashed_token: str):
+    """
+    Updates password resets for a user_id.
+    """
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    return dbc.update_doc(db_nm, USER_COLLECT, {EMAIL: user_id},
+                          {PW_RES_SALT: salt,
+                           PW_RES_TOK: hashed_token,
+                           PW_RES_TOK_ISS_TIME: tfmt.now()})
+
+
+def update_pw(user_id, salt, hashed_pw):
+    """
+    Updates a user's password (given an user_id)
+    and then blanks out the reset salt/token/issue_time,
+    so that if they need to reset again, they must get a new token.
+    """
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    return dbc.update_doc(db_nm, USER_COLLECT, {EMAIL: user_id},
+                          {SALT: salt,
+                          PASSWD: hashed_pw,
+                          PW_RES_SALT: '',
+                          PW_RES_TOK: '',
+                          PW_RES_TOK_ISS_TIME: ''})
+
+
+def update_pay_prov_sid(user_id, session_id):
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    return dbc.update_fld(
+        db_nm,
+        USER_COLLECT,
+        {EMAIL: user_id},
+        PAY_PROV_SID,
+        session_id
+    )
+
+
+def clear_pay_prov_sid(sid):
+    assert isinstance(sid, str)
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    return dbc.update_fld(
+        db_nm,
+        USER_COLLECT,
+        {PAY_PROV_SID: sid},
+        PAY_PROV_SID,
+        None
+    )
+
+
+def update_pay_prov_user_id(sid: str, pay_prov_user_id: str):
+    """
+    Identifies a user by his session id and sets his payment provider id.
+    """
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    return dbc.update_fld(
+        db_nm,
+        USER_COLLECT,
+        {PAY_PROV_SID: sid},
+        PAY_PROV_USER_ID,
+        pay_prov_user_id,
+    )
+
+
+def get_user_id(user):
+    return user.get(EMAIL, None)
+
+
+TEST_EMAIL = 'test_user@koukoudata.com'
+TEST_PAY_PROV_SID = 'test pay prov sid'
+
+
+def create_test_user():
+    return create_user(
+        TEST_EMAIL,
+        'test',
+        'user',
+        # NOTE: this password isn't hashed
+        # so this test user won't have a valid password
+        'a password',
+        'a salt',
+        'an org'
+    )
+
+
+def create_test_user_with_pay_prov_sid(sid=TEST_PAY_PROV_SID):
+    db_nm = dbc.setup_connection(dbc.USER_DB)
+    create_user(
+        TEST_EMAIL,
+        'test',
+        'user',
+        # NOTE: this password isn't hashed
+        # so this test user won't have a valid password
+        'a password',
+        'a salt',
+        'an org'
+    )
+    dbc.update_fld(
+        db_nm,
+        USER_COLLECT,
+        {EMAIL: TEST_EMAIL},
+        PAY_PROV_SID,
+        sid,
+    )
---
+++ backendcore/users/tests/test_query.py
@@ -0,0 +1,209 @@
+"""
+This module tests our code for managing users.
+"""
+
+import random
+
+import pytest
+
+import backendcore.data.db_connect as dbc
+import backendcore.users.query as usr
+
+# email for the a_user fixture:
+A_USERS_EMAIL = 'test1729@koukoudata.com'
+GARBAGE_TEST_USER = 'suhdfhjgfh'  # not even valid email addr!
+GIANT_INT = 10 ** 20
+TEST_LN = "ln"
+TEST_FN = "fn"
+TEST_SALT = "fiaklsnfef"
+TEST_ORG = "koukou inc"
+EMAIL_SUFFIX = '@someplace.com'
+VALID_PASSWD = 'Fl000by!'
+SOME_PAST_DATE = '2020/01/01'
+LAST_LOGIN_DATE = '2022/01/01'
+
+
+# We can do this globally here because it's not running on the server.
+db_nm = dbc.setup_connection(dbc.USER_DB)
+
+
+def gen_new_user_email():
+    """
+    We will generate a valid, random email address.
+    """
+    return ('user' + str(random.randint(0, GIANT_INT))
+            + EMAIL_SUFFIX)
+
+
+@pytest.fixture(scope='function')
+def a_user():
+    # let's try deleting the user first, in case some earlier test failure
+    # left this user in the db.
+    try:
+        usr.del_user(A_USERS_EMAIL)
+    except ValueError:
+        pass
+    user = usr.create_user(A_USERS_EMAIL, TEST_FN, TEST_LN,
+                           'Salted password does not matter',
+                           TEST_SALT)
+    yield user
+    usr.del_user(A_USERS_EMAIL)
+
+
+@pytest.mark.skip()
+def test_get_users_edit_grp(a_user):
+    edit_grp = usr.get_users_edit_grp(A_USERS_EMAIL)
+    assert edit_grp == usr.TEST_EDIT_GRP
+
+
+def test_list_users(a_user):
+    users = usr.list_users()
+    assert isinstance(users, list)
+    assert len(users) > 0
+
+
+def test_user_exists(a_user):
+    assert usr.user_exists(A_USERS_EMAIL)
+
+
+def test_user_not_exists(a_user):
+    assert not usr.user_exists(GARBAGE_TEST_USER)
+
+
+@pytest.mark.skip("Can't use login in users tests: reversed heirarchy!")
+def test_fetch_logins(a_user, some_logins):
+    logins = usr.fetch_logins(A_USERS_EMAIL)
+    assert isinstance(logins[usr.LOGINS], list)
+
+
+@pytest.mark.skip("Can't use login in users tests: reversed heirarchy!")
+def test_add_login(a_user, some_logins):
+    """
+    Adding a login had better increase the number of logins.
+    """
+    logins1 = usr.fetch_logins(A_USERS_EMAIL)
+    num_logins1 = len(logins1[usr.LOGINS])
+    usr.add_login(A_USERS_EMAIL)
+    logins2 = usr.fetch_logins(A_USERS_EMAIL)
+    num_logins2 = len(logins2[usr.LOGINS])
+    assert num_logins2 > num_logins1
+
+
+@pytest.mark.skip("Can't use login in users tests: reversed heirarchy!")
+def test_get_last_login(a_user, some_logins):
+    assert usr.get_last_login(A_USERS_EMAIL) == LAST_LOGIN_DATE
+
+
+def test_get_last_login_bad_user():
+    """
+    We should get a blank string for bad requests.
+    """
+    assert usr.get_last_login(A_USERS_EMAIL) == ''
+
+
+def test_update_pw_reset_token(a_user):
+    """
+    See if we reset token ok: any token value will do.
+    """
+    TEST_TOKEN = '55b748d1-0162-486d-90ab-0d6cd84b4201'
+    usr.update_pw_reset_token(A_USERS_EMAIL, TEST_SALT, TEST_TOKEN)
+    user = usr.fetch_user(A_USERS_EMAIL)
+    assert user[usr.PW_RES_TOK] == TEST_TOKEN
+
+
+def test_create_user():
+    """
+    Can we create a new user?
+    """
+    ret = usr.create_user(A_USERS_EMAIL, TEST_FN,
+                          TEST_LN, VALID_PASSWD, TEST_SALT, TEST_ORG)
+    assert ret is not None
+    usr.del_user(A_USERS_EMAIL)
+
+
+def test_create_dup_user(a_user):
+    """
+    Make sure we can't add a 2nd user with an existing user id.
+    (Ids are currently email addresses.)
+    """
+    with pytest.raises(ValueError):
+        usr.create_user(A_USERS_EMAIL, TEST_FN, TEST_LN,
+                        VALID_PASSWD, TEST_SALT, TEST_ORG)
+
+
+def test_del_no_such_user():
+    with pytest.raises(ValueError):
+        usr.del_user(GARBAGE_TEST_USER)
+
+
+def test_update_pw(a_user):
+    """
+    See if the password was updated properly.
+    Rather than try to sort through all the encryption,
+    we'll just make sure their rest token is blanked out.
+    """
+    dbc.update_fld(db_nm, usr.USER_COLLECT, {usr.EMAIL: A_USERS_EMAIL},
+                   usr.PW_RES_TOK, 'some token value')
+    usr.update_pw(A_USERS_EMAIL, TEST_SALT, 'ff3d599da14a41add6f74')
+    user = usr.fetch_user(A_USERS_EMAIL)
+    assert user[usr.PW_RES_TOK] == ''
+
+
+def test_replace_rpt_recips(a_user):
+    new_recips = ['fred@mac.com', 'bean@gmail.com']
+    usr.replace_rpt_recips(A_USERS_EMAIL, new_recips)
+    user = usr.fetch_user(A_USERS_EMAIL)
+    assert user[usr.RPT_RECIPS] == new_recips
+
+
+def test_add_rpt_recip(a_user):
+    recips = usr.add_rpt_recip(A_USERS_EMAIL, 'fred@mac.com')
+    assert 'fred@mac.com' in recips
+
+
+def test_add_rpt_recip_bad_user():
+    recips = usr.add_rpt_recip(GARBAGE_TEST_USER, 'fred@mac.com')
+    assert recips is None
+
+
+def test_add_rpt_recip_bad_recip():
+    with pytest.raises(TypeError):
+        usr.add_rpt_recip(A_USERS_EMAIL, 7)
+
+
+def test_add_rpt_recip_bad_email_format():
+    with pytest.raises(ValueError):
+        usr.add_rpt_recip(A_USERS_EMAIL, 'Not a valid email')
+
+
+def test_get_rpt_recips(a_user):
+    recips = usr.get_rpt_recips(A_USERS_EMAIL)
+    assert isinstance(recips, list)
+
+
+TEST_PAY_PROV_USER_ID = 'test pay prov user id'
+
+
+def test_update_pay_prov_sid():
+    usr.create_test_user()
+    usr.update_pay_prov_sid(usr.TEST_EMAIL, usr.TEST_PAY_PROV_SID)
+    user = usr.fetch_user(usr.TEST_EMAIL)
+    assert user[usr.PAY_PROV_SID] == usr.TEST_PAY_PROV_SID
+    usr.del_user(usr.TEST_EMAIL)
+
+
+def test_clear_pay_prov_sid():
+    usr.create_test_user()
+    usr.update_pay_prov_sid(usr.TEST_EMAIL, usr.TEST_PAY_PROV_SID)
+    usr.clear_pay_prov_sid(usr.TEST_PAY_PROV_SID)
+    user = usr.fetch_user(usr.TEST_EMAIL)
+    assert user[usr.PAY_PROV_SID] is None
+    usr.del_user(usr.TEST_EMAIL)
+
+
+def test_update_user_pay_prov_id():
+    usr.create_test_user_with_pay_prov_sid()
+    usr.update_pay_prov_user_id(usr.TEST_PAY_PROV_SID, TEST_PAY_PROV_USER_ID)
+    user = usr.fetch_user(usr.TEST_EMAIL)
+    assert user[usr.PAY_PROV_USER_ID] == TEST_PAY_PROV_USER_ID
+    usr.del_user(usr.TEST_EMAIL)
