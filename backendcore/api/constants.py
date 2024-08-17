2c5ad1d0311f8c23b29e640529bc12f732407e65
gcallah <gcallah@mac.com>
2024-08-17 18:58:46 -0400

    Changed journal to Journal.

--- backendcore/api/constants.py
+++ backendcore/api/constants.py
@@ -7,7 +7,7 @@ from backendcore.common.constants import (  # noqa F401
 AUTH = 'Authorization'
 FIELDS = 'fields'
 FORM = 'form'
-JOURNAL = 'journal'
+JOURNAL = 'Journal'
 MESSAGE = 'message'
 RETRIEVE = 'retrieve'
 TEXT = 'text'
--- backendcore/security/sec_manager2.py
+++ backendcore/security/sec_manager2.py
@@ -169,7 +169,6 @@ class SecProtocol(object):
 
 
 def is_permitted(name, action, user_id: str = '', auth_key: str = ''):
-    print(f'{user_id=}')
     prot = fetch_by_key(name)
     if not prot:
         raise ValueError(f'Unknown protocol: {name=}')
