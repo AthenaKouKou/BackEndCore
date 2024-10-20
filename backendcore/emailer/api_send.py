f5335c878c76fe18739fdd51dbcb9cd297867396
mthrndr <kristian.d.nikolov@gmail.com>
2024-10-20 16:51:02 -0400

    Added replyTo functionality to emailer

--- backendcore/emailer/api_send.py
+++ backendcore/emailer/api_send.py
@@ -1,18 +1,22 @@
 import os
 
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import (Mail, ReplyTo)
 
 DEF_SENDER = 'support@datamixmaster.com'
 
 
 def send_mail(to_emails: str, subject: str, content: str,
-              from_email=DEF_SENDER):
+              from_email=DEF_SENDER, reply_email: str = None):
     message = Mail(
         from_email=from_email,
         to_emails=to_emails,
         subject=subject,
         html_content=content)
+
+    if reply_email:
+        message.reply_to = ReplyTo(reply_email)
+
     try:
         print('About to send pw rest email using API key.')
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
