293aa01ff39b99d82357e9caeb7ceef429976389
mthrndr <kristian.d.nikolov@gmail.com>
2024-10-23 15:33:17 -0400

    Added attachents functionality to emailer

--- backendcore/emailer/api_send.py
+++ backendcore/emailer/api_send.py
@@ -1,13 +1,36 @@
 import os
+import base64
+from pathlib import Path
 
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import (Mail, ReplyTo)
+from sendgrid.helpers.mail import (
+    Attachment,
+    FileContent,
+    FileName,
+    Mail,
+    ReplyTo,
+)
 
 DEF_SENDER = 'support@datamixmaster.com'
 
 
+def encode_file(file: str):
+    with open(file, 'rb') as f:
+        data = f.read()
+        f.close()
+    return base64.b64encode(data)
+
+
+def get_attachment(file: str):
+    return Attachment(
+        FileContent(encode_file(file)),
+        FileName(Path(file).name)
+    )
+
+
 def send_mail(to_emails: str, subject: str, content: str,
-              from_email=DEF_SENDER, reply_email: str = None):
+              from_email: str = DEF_SENDER, reply_email: str = None,
+              file: str = None):
     message = Mail(
         from_email=from_email,
         to_emails=to_emails,
@@ -16,6 +39,10 @@ def send_mail(to_emails: str, subject: str, content: str,
 
     if reply_email:
         message.reply_to = ReplyTo(reply_email)
+
+    # note: file is actually the file path
+    if file:
+        message.attachment = get_attachment(file)
 
     try:
         print('About to send pw rest email using API key.')
