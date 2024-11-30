672363e035d06047960154487594f5e7c970143e
Boaz Kaufman <bk1nyc@gmail.com>
2024-11-30 12:00:24 -0500

    Modify class var names to match mongoDB return object names

--- backendcore/data/databases/common.py
+++ backendcore/data/databases/common.py
@@ -2,25 +2,25 @@
 
 class DeleteReturn():
     def __init__(self, count):
-        self.count = count
+        self.deleted_count = count
 
     def del_count(self) -> int:
-        return self.count
+        return self.deleted_count
 
     def succeeded(self) -> bool:
-        return self.count > 0
+        return self.deleted_count > 0
 
 
 class UpdateReturn():
     def __init__(self, mod_count, match_count):
-        self.mod_count = mod_count
-        self.match_count = match_count
+        self.modified_count = mod_count
+        self.matched_count = match_count
 
     def mod_count(self) -> int:
-        return self.mod_count
+        return self.modified_count
 
     def match_count(self) -> int:
-        return self.match_count
+        return self.matched_count
 
     def succeeded(self) -> bool:
-        return self.count > 0
+        return self.modified_count > 0
