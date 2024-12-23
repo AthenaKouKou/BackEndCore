722275d2f2575d136a0c61178b56761598de35ab
Boaz Kaufman <bk1nyc@gmail.com>
2024-12-23 13:04:00 -0500

    SQL read() returns object containing all records as objects (with fields)

--- backendcore/data/databases/sql_connect.py
+++ backendcore/data/databases/sql_connect.py
@@ -1,5 +1,7 @@
 import sqlalchemy as sqla
 
+from backendcore.common.constants import OBJ_ID_NM
+
 con = None
 SQLITE_DB_NM = 'SQLITE_DB_NM'
 
@@ -11,6 +13,8 @@ DB_TABLE = {
 }
 
 engine = None
+
+NO_SORT = 0
 
 
 class SqlDB():
@@ -35,20 +39,30 @@ class SqlDB():
         print(record)
         # 'Begin once' mode - so we don't need to explicitly commit every time
         with engine.begin() as conn:
-            conn.execute(sqla.text(f"CREATE TABLE {table} (x int, y int)"))
+            conn.execute(sqla.text(f"CREATE TABLE {table} (_id int, x int, y int)"))
             conn.execute(
-                sqla.text(f"INSERT INTO {table} (x, y) VALUES (:x, :y)"),
-                [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+                sqla.text(f"INSERT INTO {table} (_id, x, y) VALUES (:_id, :x, :y)"),
+                [{"_id": 0, "x": 1, "y": 1}, {"_id": 1, "x": 2, "y": 4}],
             )
 
+    # def read(self, db_nm, clct_nm, sort=NO_SORT,
+    #          sort_fld=OBJ_ID_NM, no_id=False):
     def read(self):
         # result._metadata.keys to get field names.
         recs = []
+        fields = []
+        all_docs = {}
         with engine.connect() as conn:
             res = conn.execute(sqla.text("SELECT * from some_table"))
+            for field in res._metadata.keys:
+                fields.append(field)
             for rec in res:
-                recs.append(rec)
-        return recs
+                doc = {}
+                for i in range(len(fields)):
+                    doc[fields[i]] = rec[i]
+                id = doc[OBJ_ID_NM]
+                all_docs[id] = doc
+        return all_docs
 
     def update(self):
         pass
@@ -59,9 +73,8 @@ class SqlDB():
 
 def main():
     sqlDB = SqlDB()
-    sqlDB.create({})
-    print(sqlDB.read())
-
+    sqlDB.create('some_table', {})
+    print(f'recs = {sqlDB.read()}')
 
 if __name__ == '__main__':
     main()
