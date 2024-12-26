01d974f9c14ba41671f028eb5ede40b57d994ad6
Boaz Kaufman <bk1nyc@gmail.com>
2024-12-26 18:05:42 -0500

    Create new sql tables using sqla schema

--- backendcore/data/databases/sql_connect.py
+++ backendcore/data/databases/sql_connect.py
@@ -1,4 +1,7 @@
 import sqlalchemy as sqla
+from icecream import ic
+
+from backendcore.common.constants import OBJ_ID_NM
 
 con = None
 SQLITE_DB_NM = 'SQLITE_DB_NM'
@@ -22,12 +25,28 @@ class SqlDB():
     def __init__(self, variant=SQLITE_MEM) -> None:
         self.variant = variant
         global engine
+        self.mdata = sqla.MetaData()
         if engine is None:
             engine = self._connectDB()
 
     def _connectDB(self):
         connect_str = DB_TABLE[self.variant]
         return sqla.create_engine(connect_str, echo=True)
+    
+    def _get_metadata(self):
+        return self.mdata
+
+    def create_table(self, table_name, columns=None):
+        # Sets up a new table schema and creates it in the DB
+        new_table = sqla.Table(
+            table_name,
+            self.mdata,
+            sqla.Column(OBJ_ID_NM, sqla.Integer, primary_key=True),
+        )
+        for column in columns:
+            new_table.append_column(sqla.Column(column[0], column[1]))
+        self.mdata.create_all(engine)
+        return new_table
 
     def create(self, table: str, record: dict):
         """
@@ -35,11 +54,13 @@ class SqlDB():
         Return?
         """
         print(record)
+        table_cols = [
+            ('x', sqla.Integer),
+            ('y', sqla.Integer),
+        ]
+        new_table = self.create_table(table, table_cols)
         # 'Begin once' mode - so we don't need to explicitly commit every time
         with engine.begin() as conn:
-            conn.execute(sqla.text(
-                f"CREATE TABLE {table} (_id int, x int, y int)")
-                )
             res = conn.execute(
                 sqla.text(f"INSERT INTO {table} \
                           (_id, x, y) VALUES (:_id, :x, :y)"),
@@ -47,7 +68,7 @@ class SqlDB():
             )
             return res
 
-    def read_recs_to_objs(self, res):
+    def _read_recs_to_objs(self, res):
         """
         Transforms results from a SELECT
         into objects with fields.
@@ -70,10 +91,11 @@ class SqlDB():
         Returns all docs from a collection.
         `sort` can be DESC, NO_SORT, or ASC.
         """
+        # ic(db_nm, sort, sort_fld, no_id)
         all_docs = []
         with engine.connect() as conn:
             res = conn.execute(sqla.text(f"SELECT * from {clct_nm}"))
-            all_docs = self.read_recs_to_objs(res)
+            all_docs = self._read_recs_to_objs(res)
         return all_docs
 
     def update(self):
