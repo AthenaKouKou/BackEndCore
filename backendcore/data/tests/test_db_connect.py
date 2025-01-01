ea6cb6d444a575972912b1122d1a227336edaa6b
Boaz Kaufman <bk1nyc@gmail.com>
2025-01-01 18:45:32 -0500

    Update dbc tests to work with nonmongo DBs

--- backendcore/data/tests/test_db_connect.py
+++ backendcore/data/tests/test_db_connect.py
@@ -7,9 +7,9 @@ from copy import deepcopy
 
 from unittest.mock import patch
 import pytest
+import os
 
 import backendcore.data.db_connect as dbc
-import backendcore.data.databases.mongo_connect as mdb
 
 TEST_DB = 'test_db'
 TEST_COLLECT = 'test_collect'
@@ -30,8 +30,16 @@ BAD_VAL = "Scooby-dooby-doo!"
 
 BIG_INT = 10**32
 
-MONGO_DB_LOC = 'backendcore.data.databases.mongo_connect'
+DBS_LOC = 'backendcore.data.databases'
+MONGO_DB_LOC = f'{DBS_LOC}.mongo_connect'
 MONGO_DB_OBJ = f'{MONGO_DB_LOC}.MongoDB'
+SQL_DB_LOC = f'{DBS_LOC}.sql_connect'
+SQL_DB_OBJ = f'{SQL_DB_LOC}.SqlDB'
+
+DB_OBJ = MONGO_DB_OBJ
+db_type = os.environ.get('DATABASE', dbc.MONGO)
+if db_type == dbc.SQL:
+    DB_OBJ = SQL_DB_OBJ
 
 
 def rand_fld_val():
@@ -41,7 +49,7 @@ def rand_fld_val():
     return str(random.randint(0, BIG_INT))
 
 
-@patch(f'{MONGO_DB_OBJ}.read_one', autospec=True, return_value={})
+@patch(f'{DB_OBJ}.read_one', autospec=True, return_value={})
 def test_read_one_no_filter(mock_read_one):
     """
     Tests that a fetch with no filter retrieves the rec we inserted
@@ -51,7 +59,7 @@ def test_read_one_no_filter(mock_read_one):
     assert rec is not None
 
 
-@patch(f'{MONGO_DB_OBJ}.read_one', autospec=True, return_value=None)
+@patch(f'{DB_OBJ}.read_one', autospec=True, return_value=None)
 def test_read_one_bad_filter(mock_read_one):
     """
     Tests that a fetch with a bad filter fails.
@@ -60,7 +68,7 @@ def test_read_one_bad_filter(mock_read_one):
     assert rec is None
 
 
-@patch(f'{MONGO_DB_OBJ}.read_one', autospec=True, return_value={})
+@patch(f'{DB_OBJ}.read_one', autospec=True, return_value={})
 def test_read_one_good_filter(mock_read_one):
     """
     Tests that a fetch with a good filter works.
@@ -69,26 +77,26 @@ def test_read_one_good_filter(mock_read_one):
     assert rec is not None
 
 
-@patch(f'{MONGO_DB_OBJ}.fetch_by_id', autospec=True, return_value={})
+@patch(f'{DB_OBJ}.fetch_by_id', autospec=True, return_value={})
 def test_fetch_by_id(mock_fetch_by_id):
     ret = dbc.fetch_by_id(TEST_DB, TEST_COLLECT, {'any': 'vals'})
     assert ret is not None
 
 
-@patch(f'{MONGO_DB_OBJ}.delete_by_id', autospec=True,
+@patch(f'{DB_OBJ}.delete_by_id', autospec=True,
        return_value='Not none')
 def test_delete_by_id(mock_delete_by_id):
     assert dbc.delete_by_id(TEST_DB, TEST_COLLECT, 'some id') is not None
 
 
-@patch(f'{MONGO_DB_OBJ}.update_fld', autospec=True, return_value=RET_CONST)
+@patch(f'{DB_OBJ}.update_fld', autospec=True, return_value=RET_CONST)
 def test_update_fld(a_doc):
     assert dbc.update_fld(TEST_DB, TEST_COLLECT,
                           DEF_PAIR, DEF_FLD,
                           'Any value') == RET_CONST
 
 
-@patch(f'{MONGO_DB_OBJ}.update', autospec=True, return_value=RET_CONST)
+@patch(f'{DB_OBJ}.update', autospec=True, return_value=RET_CONST)
 def test_update(mock_update):
     """
     Just testing we get back the return of the installed db.
@@ -97,7 +105,7 @@ def test_update(mock_update):
     assert ret == RET_CONST
 
 
-@patch(f'{MONGO_DB_OBJ}.upsert', autospec=True, return_value=RET_CONST)
+@patch(f'{DB_OBJ}.upsert', autospec=True, return_value=RET_CONST)
 def test_upsert(mock_upsert):
     """
     Just testing we get back the return of the installed db.
@@ -157,19 +165,19 @@ def test_num_updated_none(a_doc):
     assert dbc.num_updated(result) == 0
 
 
-@patch(f'{MONGO_DB_OBJ}.select', autospec=True, return_value={})
+@patch(f'{DB_OBJ}.select', autospec=True, return_value={})
 def test_select(mock_select):
     recs = dbc.select(TEST_DB, TEST_COLLECT, filters={})
     assert isinstance(recs, dict)
 
 
-@patch(f'{MONGO_DB_OBJ}.delete', autospec=True,
+@patch(f'{DB_OBJ}.delete', autospec=True,
        return_value='Not none')
 def test_delete(mock_delete):
     assert dbc.delete(TEST_DB, TEST_COLLECT) is not None
 
 
-@patch(f'{MONGO_DB_OBJ}.delete_many', autospec=True,
+@patch(f'{DB_OBJ}.delete_many', autospec=True,
        return_value='Not none')
 def test_delete_many(mock_delete_many):
     """
@@ -228,7 +236,7 @@ def test_num_deleted_none(a_doc):
     assert dbc.num_deleted(result) == 0
 
 
-@patch(f'{MONGO_DB_OBJ}.add_fld_to_all', autospec=True, return_value=RET_CONST)
+@patch(f'{DB_OBJ}.add_fld_to_all', autospec=True, return_value=RET_CONST)
 def test_add_fld_to_all(mock_add_fld_to_all):
     """
     Testing updating all records with some new field.
@@ -237,7 +245,7 @@ def test_add_fld_to_all(mock_add_fld_to_all):
     assert ret == RET_CONST
 
 
-@patch(f'{MONGO_DB_OBJ}.append_to_list', autospec=True, return_value=RET_CONST)
+@patch(f'{DB_OBJ}.append_to_list', autospec=True, return_value=RET_CONST)
 def test_append_to_list(mock_append_to_list):
     """
     Test appending to an interior doc list.
@@ -246,7 +254,7 @@ def test_append_to_list(mock_append_to_list):
                               'any list', 'any list val') == RET_CONST
 
 
-@patch(f'{MONGO_DB_OBJ}.rename', autospec=True, return_value=RET_CONST)
+@patch(f'{DB_OBJ}.rename', autospec=True, return_value=RET_CONST)
 def test_rename(some_docs):
     """
     Test renaming a field.
