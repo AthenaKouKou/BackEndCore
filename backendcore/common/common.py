699d2e911e8c53b4ba0a3239afc41b6d2e27465c
mthrndr <kristian.d.nikolov@gmail.com>
2025-02-18 15:27:46 -0500

    Updated common.common

--- backendcore/common/common.py
+++ backendcore/common/common.py
@@ -7,3 +7,7 @@ client_code = None
 def get_client_code():
     global client_code
     client_code = os.getenv(CLIENT_CODE)
+    if client_code is not None:
+        return client_code
+    else
+        return ""
