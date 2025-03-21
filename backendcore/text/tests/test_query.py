9e39dce030c5191df11dc6a4344a38507b451756
Boaz Kaufman <bk1nyc@gmail.com>
2025-03-21 09:49:45 -0400

    Update text query tests

--- backendcore/text/tests/test_query.py
+++ backendcore/text/tests/test_query.py
@@ -2,106 +2,47 @@ from copy import deepcopy
 
 import pytest
 
-import backendcore.text.query as qry
-
-
-def del_test_item(code):
-    """
-    Delete by code.
-    """
-    return qry.delete(code)
-
-
-def get_sample():
-    return deepcopy(qry.TEST_SAMPLE)
-
-
-def add_test_sample():
-    return qry.add(get_sample())
+import text.query as qry
 
 
 @pytest.fixture(scope='function')
-def temp_sample():
-    try:  # in case some failed test left it hanging on...
-        del_test_item(qry.TEST_CODE)
-    except Exception:
-        print(f'{qry.TEST_CODE} was not present')
-    sample_dict = deepcopy(qry.TEST_SAMPLE)
-    ret = qry.add(sample_dict)
+def temp_text():
+    text_dict = deepcopy(qry.TEST_TEXT)
+    ret = qry.add(text_dict)
     yield ret
-    del_test_item(qry.TEST_CODE)
+    qry.delete(qry.TEST_TITLE)
 
 
-@pytest.fixture(scope='function')
-def new_sample():
-    """
-    Creates a entry, but does not delete it after test runs.
-    """
-    return add_test_sample()
-
-
-def test_fetch_codes(temp_sample):
+def test_fetch_codes(temp_text):
     codes = qry.fetch_codes()
     assert isinstance(codes, list)
-    assert qry.TEST_CODE in codes
+    assert qry.TEST_TITLE in codes
 
 
-def test_fetch_list(temp_sample):
-    samples = qry.fetch_list()
-    assert isinstance(samples, list)
-    assert len(samples) > 0
+def test_fetch_list(temp_text):
+    texts = qry.fetch_list()
+    assert isinstance(texts, list)
+    assert len(texts) > 0
 
 
-def test_fetch_dict(temp_sample):
-    samples = qry.fetch_dict()
-    assert isinstance(samples, dict)
-    assert len(samples) > 0
+def test_fetch_dict(temp_text):
+    texts = qry.fetch_dict()
+    assert isinstance(texts, dict)
+    assert len(texts) > 0
 
 
-def test_get_choices(temp_sample):
-    choices = qry.get_choices()
-    assert qry.TEST_CODE in choices
+NEW_TEXT = 'Some new text'
+NEW_ED = 'new@ed.com'
 
 
-def test_fetch_by_key(temp_sample):
-    entry = qry.fetch_by_key(qry.TEST_CODE)
-    assert entry[qry.CODE] == qry.TEST_CODE
+def test_update_text(temp_text):
+    qry.update(qry.TEST_TITLE, NEW_TEXT, NEW_ED)
+    new_rec = qry.fetch_by_key(qry.TEST_TITLE)
+    assert new_rec[qry.TEXT] == NEW_TEXT
+    assert new_rec[qry.EDITOR] == NEW_ED
 
 
-def test_fetch_by_key_not_there():
-    assert qry.fetch_by_key('A Very Unlikely Term') is None
-
-
-def test_add():
-    qry.add(get_sample())
-    assert qry.fetch_by_key(qry.TEST_CODE) is not None
-    del_test_item(qry.TEST_CODE)
-
-
-def test_add_dup_term(temp_sample):
+def test_update_text_bad_title(temp_text):
+    NEW_TEXT = 'Some new text'
     with pytest.raises(ValueError):
-        qry.add(get_sample())
-
-
-def test_delete(new_sample):
-    qry.delete(qry.TEST_CODE)
-    assert qry.fetch_by_key(qry.TEST_CODE) is None
-
-
-def test_delete_not_there():
-    with pytest.raises(ValueError):
-        qry.delete('not an existing code')
-
-
-def test_update(temp_sample):
-    NEW_NAME = 'A new name'
-    update_dict = {qry.NAME: NEW_NAME}
-    assert qry.fetch_by_key(qry.TEST_CODE)[qry.NAME] != NEW_NAME
-    qry.update(qry.TEST_CODE, update_dict)
-    assert qry.fetch_by_key(qry.TEST_CODE)[qry.NAME] == NEW_NAME
-
-
-def test_update_not_there():
-    update_dict = {qry.NAME: 'something'}
-    with pytest.raises(ValueError):
-        qry.update('not an existing code', update_dict)
+        qry.update('Not an existing title', NEW_TEXT, NEW_ED)
