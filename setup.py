641cff04c591eb863e91e55ebbcdb57c8f069f13
gcallah <gcallah@mac.com>
2025-05-20 18:56:06 -0400

    Bumped version number to 1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BackEndCore",
-    version="1.3",
+    version="1.4",
     description="Core code for DataMixMaster's backends.",
     packages=find_packages(),
     url="https://github.com/AthenaKouKou/BackEndCore",
