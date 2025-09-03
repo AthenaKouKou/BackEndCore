81a0453cba30366a83ccfb73ac219bb8e1c16e2a
gcallah <gcallah@mac.com>
2025-09-03 00:24:02 -0400

    Only get user_id in sec manager2 if auth key is present.

--- backendcore/security/sec_manager2.py
+++ backendcore/security/sec_manager2.py
@@ -301,7 +301,7 @@ def is_permitted(prot_name, action, user_id: str = '', auth_key: str = '',
     if not prot:
         raise ValueError(f'Unknown protocol: {prot_name=}')
     check_vals = {}
-    if not user_id:
+    if not user_id and auth_key:
         user_id = ak.fetch_user_id_by_key(auth_key)
     check_vals[VALIDATE_USER] = user_id
     check_vals[AUTH_KEY] = auth_key
